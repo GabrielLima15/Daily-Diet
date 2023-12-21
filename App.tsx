6b05603251d089a8de5da0a58b44e8ea2ae22b68
Gabriel Carvalho de Lima <77641643+GabrielLima15@users.noreply.github.com>
2023-12-21 00:26:51 -0300

    First Commit 🚀

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from 'styled-components/native'
+import theme from './src/theme';
+
+export default function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			
+		</ThemeProvider>
+	);
+}
+
---
+++ src/theme/index.ts
@@ -0,0 +1,39 @@
+export default {
+	colors: {
+		green: '#22B4B1',
+		green_for_svg: '#74DB8C',
+		green_background_svg: '#BDF4DE',
+		red_for_svg: '#EE0F0F',
+		red_background_svg: '#FED1CB',
+		orange: '#F39200',
+		yellow: '#EFBC06',
+		white: '#FFFFFF',
+		gray: '#F0F0F0',
+		black: '#000',
+		border: '#BCC5E3',
+
+		input_text: '#475C98',
+		input_text_placeholder: '#9CAAD1',
+		text: '#575656',
+		group_circle: '#696969',
+	},
+
+	fonts: {
+		inter_bold: 'Inter-Bold',
+		inter_medium: 'Inter-Medium',
+		inter_regular: 'Inter-Regular',
+		inter_semibold: 'Inter-SemiBold',
+
+		poppins_bold: 'Poppins-Bold',
+		poppins_medium: 'Poppins-Medium',
+		poppins_regular: 'Poppins-Regular',
+		poppins_semibold: 'Poppins-SemiBold',
+	},
+
+	font_size: {
+		SM: 14,
+		MD: 16,
+		LG: 18,
+		XL: 24,
+	},
+};
