4c65b2bef174f53bae9953683e399738074e2812
GabrielLima15 <gabrielcarvalho850@outlook.com>
2024-04-25 21:42:25 -0300

    fix: :bug: Back Button disable

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -3,4 +3,5 @@ import styled from "styled-components/native";
 
 export const Container = styled.TouchableOpacity`
 	margin: ${getResponsiveSpacing(0)}px ${getResponsiveSpacing(8)}px;
+	z-index: 10;
 `
