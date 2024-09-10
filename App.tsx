7b8c35626d91304ffc7eec8cd8c54197a3e59c66
GabrielLima15 <gabrielcarvalho850@outlook.com>
2024-09-09 22:34:52 -0300

    refatorado core do app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,48 @@
-import { ThemeProvider } from 'styled-components/native'
+import React, { useEffect, useState } from 'react';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons'; 
+import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import AppContext from '@context/index';
+import Router from '@router/index';
+import Toast from 'react-native-toast-message';
+import ModalController from '@components/Modal/Controller';
 import theme from '@theme/index';
-
-import {
-	NunitoSans_400Regular,
-	NunitoSans_700Bold,
-} from '@expo-google-fonts/nunito-sans'
-import { useFonts } from 'expo-font';
-import { Routes } from './src/routes';
-import { StatusBar } from 'react-native';
-import React from 'react';
-import { Loading } from './src/components/Loading';
+import { navigationRef } from '@router/root';
+import { loadFonts } from '@assets/fonts';
+import { Loading } from '@components/Loading';
 
 export default function App() {
-	const [fontLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
+  const [loading, setLoading] = useState(true);
 
-	return (
-		<ThemeProvider theme={theme}>
-			{fontLoaded ? <Routes /> : <Loading />}
-			<StatusBar backgroundColor="transparent" translucent barStyle='dark-content' />
-		</ThemeProvider>
-	);
+  const initApplication = async () => {
+    await loadFonts();
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    initApplication();
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} /> 
+      <ApplicationProvider {...eva} theme={eva.light}> 
+        <NavigationContainer ref={navigationRef}>
+          <AppContext>
+            <ThemeProvider theme={theme}>
+              <Router />
+              <ModalController />
+              <Toast />
+            </ThemeProvider>
+          </AppContext>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
+  );
 }
-
