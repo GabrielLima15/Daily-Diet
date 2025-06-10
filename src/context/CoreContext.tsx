be1abda2418361fadc6d0a1e229cf5e65f9bcec9
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-06-10 15:10:05 -0300

    removido diet do coreContext

--- src/context/CoreContext.tsx
+++ src/context/CoreContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, ReactNode, useContext, Dispatch, SetStateAction } from 'react'
+import React, { useState, createContext, ReactNode, useContext, Dispatch, SetStateAction } from 'react';
 
 interface ModalState {
   type?: 'confirm' | string;
@@ -6,53 +6,37 @@ interface ModalState {
   action?: () => void;
 }
 
-interface DietState {
-  id: string
-  name: string
-  description: string
-  data: string
-  hora: string
-  insideDiet: boolean
-}
-
 interface CoreContextType {
   modal: ModalState | null;
   setModal: Dispatch<SetStateAction<ModalState | null>>;
-  diet: DietState | null;
-  setDiet: Dispatch<SetStateAction<DietState | null>>;
 }
 
 export const CoreContext = createContext<CoreContextType>({
   modal: null,
   setModal: () => {},
-  diet: null,  
-  setDiet: () => {} 
-})
+});
 
 interface CoreStateProps {
   children: ReactNode;
 }
 
 export const CoreState = ({ children }: CoreStateProps) => {
-  const [modal, setModal] = useState<ModalState | null>(null)
-  const [diet, setDiet] = useState<DietState | null>(null)
+  const [modal, setModal] = useState<ModalState | null>(null);
 
   const contextValue = {
     modal,
     setModal,
-    diet,
-    setDiet,
-  }
+  };
 
-  return <CoreContext.Provider value={contextValue}>{children}</CoreContext.Provider>
-}
+  return <CoreContext.Provider value={contextValue}>{children}</CoreContext.Provider>;
+};
 
 export const useCoreContext = () => {
-  const context = useContext(CoreContext)
+  const context = useContext(CoreContext);
 
   if (!context) {
-    throw new Error('useCoreContext must be used within a CoreState provider')
+    throw new Error('useCoreContext must be used within a CoreState provider');
   }
 
-  return context
-}
+  return context;
+};
