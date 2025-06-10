f66ecaff371eb5a1501c9b767c3a86348377f8d6
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-06-10 15:09:52 -0300

    update

--- src/services/storage.tsx
+++ src/services/storage.tsx
@@ -2,14 +2,24 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const storageKey = 'Stor:Gabriel:@1.0.0'
 
-export const SaveObject = async (key : string, value : string | null) => {
-    return await SaveStorage(key, JSON.stringify(value))
-}
+export const SaveObject = async (key: string, value: any) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(`${storageKey}::${key}`, jsonValue);
+  } catch (error) {
+    console.error('Error saving data to AsyncStorage', error);
+  }
+};
 
-export const ReadObject = async (key : string) => {
-    const item = await ReadStorage(key);
-    return item ? JSON.parse(item) : null;
-}
+export const ReadObject = async (key: string) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(`${storageKey}::${key}`);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (error) {
+    console.error('Error reading data from AsyncStorage', error);
+    return null;
+  }
+};
 
 export const SaveStorage = async (key : string, value : any) => {
     return await AsyncStorage.setItem(`${ storageKey }::${ key }`, value)
